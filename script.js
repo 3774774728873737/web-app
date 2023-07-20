036d487d5b97bca80ade702800407b84da78e18d
3774774728873737 <138348862+3774774728873737@users.noreply.github.com>
2023-07-20 17:59:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// Declare a global array variable to store the uploaded video files
+var uploadedVideos = [];
+
+// Function to handle file upload for a specific video
 function handleFileUpload(videoNumber) {
+  console.log("runs");
+
   var fileInput = document.getElementById('fileInput' + videoNumber);
-  
+
   fileInput.onchange = function () {
-    var files = fileInput.files;
-    
+    var file = fileInput.files[0];
+    var fileURL = URL.createObjectURL(file);
 
     var videoPlayer = document.getElementById('videoPlayer' + videoNumber);
     var videoThumbnail = document.getElementById('videoThumbnail' + videoNumber);
@@ -14,31 +20,41 @@ function handleFileUpload(videoNumber) {
     videoThumbnail.style.height = '100%'; // Set the height to 540 pixels
     videoThumbnail.src = ''; // Clear the previous thumbnail
     videoThumbnail.alt = ''; // Clear the alt text
-    
     // Set placeholder image
     videoThumbnail.src = 'load.png';
 
+    
     var formData = new FormData();
-    for (var i = 0; i < files.length; i++) {
-      formData.append('files', files[i]);
-    }
+    formData.append('file', file);
     formData.append('videoNumber', videoNumber); // Add the videoNumber to the formData
 
-    fetch('https://fast-api-script-0d0703f4a5dc.herokuapp.com/upload', {
+    console.log("data23")
+
+    fetch('http://localhost:8000/upload', {
       method: 'POST',
       body: formData,
     })
       .then(response => response.json())
       .then(data => {
-        console.log(data.message); // Display the message
+        console.log("asdasd", data.message); // Display the message
+        console.log("DSADAS", data.imagePath);
 
         if (data.imagePath) {
-          // Add a cache-busting parameter to the image URL
-          var imageURL = data.imagePath + '?' + new Date().getTime();
-          videoThumbnail.src = imageURL; // Replace placeholder with actual image
+          // Set the thumbnail image source to the base64 data
+          var videoThumbnail = document.getElementById('videoThumbnail' + videoNumber);
+          console.log("DSADAS", data.imagePath);
+          videoThumbnail.src = 'data:image/jpeg;base64,' + data.imagePath;
+          videoThumbnail.style.display = 'block';
+          videoThumbnail.style.width = '100%'; // Set the width to 450 pixels
+          videoThumbnail.style.height = '100%'; // Set the height to 540 pixels
         } else {
-          videoThumbnail.src = 'load.png'; // Keep placeholder if no thumbnail available
+          // Hide the image tag if no thumbnail available
+          var videoThumbnail = document.getElementById('videoThumbnail' + videoNumber);
+          videoThumbnail.style.display = 'none';
         }
+
+        // Add the uploaded file to the array
+        uploadedVideos.push(file);
       })
       .catch(error => {
         console.error('Error:', error);
@@ -48,33 +64,14 @@ function handleFileUpload(videoNumber) {
   fileInput.click();
 }
 
-
-function handleAudioUpload() {
-  var audioInput = document.getElementById('audioInput');
-  var audioFile = audioInput.files[0];
-
-  var formData = new FormData();
-  formData.append('file', audioFile);
-
-  fetch('https://fast-api-script-0d0703f4a5dc.herokuapp.com/upload-audio', {
-    method: 'POST',
-    body: formData,
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.message); // Display the message
-      // Perform any desired actions with the response from the server
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
-
-// Function to upload the audio file
-function uploadAudio() {
-  var audioInput = document.getElementById('audioInput');
-  audioInput.onchange = handleAudioUpload;
-  audioInput.click();
+// Function to handle file upload for video 1
+function uploadFile(videoNumber) {
+  var fileInput = document.getElementById('fileInput' + videoNumber);
+  fileInput.onchange = function () {
+    handleFileUpload(videoNumber);
+  };
+  fileInput.click();
+  fileInput.value = '';
 }
 
 // Function to handle file upload for video 2 and video 3
@@ -86,13 +83,106 @@ function chooseFile(videoNumber) {
   fileInput.click();
 }
 
+function handleAudioUpload() {
+
+  console.log("runsauido")
+  var audioInput = document.getElementById('audioInput');
+  var audioFile = audioInput.files[0];
+
+  var formData = new FormData();
+  formData.append('file', audioFile);
+
+}
+
+
+
+function uploadAudio() {
+  var audioInput = document.getElementById('audioInput');
+
+  audioInput.onchange = function () {
+    handleAudioUpload();
+  };
+  audioInput.click();
+  audioInput.value = ''; // Reset the value of the audio input
+}
 // Add event listeners to the file input elements when the page is loaded
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var fileInputs = document.querySelectorAll("input[type='file']");
-  fileInputs.forEach(function(fileInput) {
-    fileInput.addEventListener('change', function() {
+  fileInputs.forEach(function (fileInput) {
+    fileInput.addEventListener('change', function () {
       var videoNumber = this.id.replace("fileInput", "");
       handleFileUpload(videoNumber);
     });
   });
 });
+
+function combine() {
+  var loadedVideos = 0;
+  var videosToLoad = 3; // Minimum number of videos required
+
+  // Check if each video is loaded
+  for (var i = 1; i <= 3; i++) {
+    var videoThumbnail = document.getElementById('videoThumbnail' + i);
+    if (videoThumbnail.src.includes('html')) {
+      // Skip if the video is not loaded
+      continue;
+    }
+    loadedVideos++;
+  }
+
+  if (loadedVideos === videosToLoad) {
+    var loadingOverlay = document.getElementById('loadingOverlay');
+    loadingOverlay.classList.add('active');
+    var text = ""
+    // Create a new FormData object and append all uploaded videos even if multiple files are selected
+    var formData = new FormData();
+    for (var i = 1; i <= 3; i++) {
+      var fileInput = document.getElementById('fileInput' + i);
+      if (fileInput.files.length > 0) {
+        // Loop through all selected files and append them
+        for (var j = 0; j < fileInput.files.length; j++) {
+          text += i + ","
+          var file = fileInput.files[j];
+          formData.append('files', file);
+        }
+      }
+
+      formData.append('videoNumber', text);
+
+    }
+    // Append the audio file if available
+    var audioInput = document.getElementById('audioInput');
+    if (audioInput.files.length > 0) {
+      var audioFile = audioInput.files[0];
+      formData.append('audio', audioFile);
+    }
+
+    // Upload the videos and combine them
+    fetch('http://localhost:8000/combine', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.blob();
+        } else {
+          throw new Error('Unable to combine videos');
+        }
+      })
+      .then(blob => {
+        var downloadLink = document.createElement('a');
+        downloadLink.href = URL.createObjectURL(blob);
+        downloadLink.download = 'combined.mp4';
+
+        downloadLink.click();
+        loadingOverlay.classList.remove('active'); // Hide the loading overlay
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        loadingOverlay.classList.remove('active'); // Hide the loading overlay
+      });
+  } else {
+    // Show a dialog box with the error message
+    alert('All videos must be loaded');
+  }
+}
